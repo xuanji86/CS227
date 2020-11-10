b553438aaee750cc8032a8f3a41a88425915c446
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2020-11-10 01:45:33 -0600

    1

---
+++ project8/src/library/libraryItem.java
@@ -0,0 +1,14 @@
+package library;
+
+import java.util.Date;
+
+public abstract class libraryItem {
+	
+	private String title;
+	
+	public libraryItem(String title) {
+		this.title = title;
+	}
+	
+
+}
