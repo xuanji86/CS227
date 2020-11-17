bc5ed2df6c0c0d1bcaa9b46ac6a85c7ae911c207
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2020-11-17 01:46:49 -0600

    1

--- hw4_sample_code/src/hw4/FullAdder.java
+++ hw4_sample_code/src/hw4/FullAdder.java
@@ -19,6 +19,16 @@ public class FullAdder extends CompoundComponent {
 		addComponent​(halfAdder2);
 		addComponent​(orGate);
 		
+		inputs()[0].connectTo(halfAdder1.inputs()[0]);
+		inputs()[1].connectTo(halfAdder1.inputs()[1]);
+		inputs()[2].connectTo(halfAdder2.inputs()[0]);
+		
+		halfAdder1.outputs()[0].connectTo(halfAdder2.inputs()[1]);
+		halfAdder1.outputs()[1].connectTo(orGate.inputs()[0]);
+		halfAdder2.outputs()[1].connectTo(orGate.inputs()[1]);
+		
+		halfAdder2.outputs()[0].connectTo(outputs()[0]);
+		orGate.outputs()[0].connectTo(outputs()[1]);
 	}
 	
 
---
+++ hw4_sample_code/src/hw4/Register.java
@@ -0,0 +1,30 @@
+package hw4;
+
+import api.IStatefulComponent;
+
+public class Register extends AbstractComponent implements IStatefulComponent{
+
+	public Register(int size) {
+		super(size,size);
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
