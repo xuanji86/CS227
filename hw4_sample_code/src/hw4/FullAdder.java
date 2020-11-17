fb8da10bd3ea886c1514e16101c1dff11932e39b
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2020-11-16 22:40:49 -0600

    1

--- hw4_sample_code/src/hw4/FullAdder.java
+++ hw4_sample_code/src/hw4/FullAdder.java
@@ -1,10 +1,24 @@
 package hw4;
 
-public class FullAdder extends CompoundComponent {
+import api.IComponent;
 
+public class FullAdder extends CompoundComponent {
+	
+	private IComponent halfAdder1;
+	private IComponent halfAdder2;
+	private IComponent orGate;
+	
 	public FullAdder() {
 		super(3, 2);
-
+		
+		halfAdder1 = new HalfAdder();
+		halfAdder2 = new HalfAdder();
+		orGate = new OrGate();
+		
+		addComponent​(halfAdder1);
+		addComponent​(halfAdder2);
+		addComponent​(orGate);
+		
 	}
 	
 
